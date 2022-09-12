d29d53f286121940863fd8bb44cc0cc61ab572f7
toshi <toshi.sakao.r@gmail.com>
2022-09-13 02:34:12 +0900

    did one more kattis

--- kattis-kant/src/main.rs
+++ kattis-kant/src/main.rs
@@ -1,32 +1,26 @@
-use std::{io, vec};
+use std::io;
 
 fn main() {
     // get standard input stream
     let mut input = String::new();
 
     io::stdin().read_line(&mut input).expect("Failed read 1");
-    let mut rectsize: Vec<usize> = input
+    let rectsize: Vec<usize> = input
         .trim()
         .split(" ")
         .map(|x| x.parse().expect("Could not parse input"))
         .collect();
 
-    for y in 1..(rectsize[1] + 1) {
-        for x in 1..(rectsize[0] + 1) {
-            eprint!("{}", fastest_way(x, y, &rectsize));
+    for x in 1..(rectsize[0] + 1) {
+        for y in 1..(rectsize[1] + 1) {
+            print!("{}", fastest_way(x, y, &rectsize));
         }
-        eprint!("\n");
+        print!("\n");
     }
-
-    
-
     // println!("{}", answer);
 }
 
-fn fastest_way(mut x: usize, mut y: usize, size:&Vec<usize>) -> u32 {
-    //check x and y and compare which is smallest and return smallest
-
-    // (4 - 5) + 1= 2
+fn fastest_way(mut x: usize, mut y: usize, size:&Vec<usize>) -> String {
     if x > (size[0] / 2) {
         x = size[0] - x + 1;
     }
@@ -34,7 +28,13 @@ fn fastest_way(mut x: usize, mut y: usize, size:&Vec<usize>) -> u32 {
         y = size[1] - y + 1;
     }
     if x <= y {
-        return x as u32;
+        if x > 9 {
+            return ".".to_string();
+        }
+        return x.to_string();
     }
-    return y as u32;
+    if y > 9 {
+        return ".".to_string();
+    }
+    return y.to_string();
 }
